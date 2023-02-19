3d29ff535ba881d35b26e790f9854910518ebe39
Edmund Quainoo <edmundquainoo3018@gmail.com>
2023-02-19 17:34:04 +0000

    Still an info

--- assignment.dart
+++ assignment.dart
@@ -1,4 +1,4 @@
 void main() {
   print(
-      "I am collecting my assignment tonight\n i want you guys to push all 10 assignments to your branch  ");
+      "I am collecting my assignment tonight\n I want you guys to push all 10 assignments to your branch that I will merge all  ");
 }
